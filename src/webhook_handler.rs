72881c4b60427ebad29e485d9ae698359d395f08
TTF-fog <74908128+TTF-fog@users.noreply.github.com>
2024-10-19 19:23:52 +0530

    feat: webhook send image

--- src/webhook_handler.rs
+++ src/webhook_handler.rs
@@ -1,3 +1,28 @@
-pub fn send_image(){
+use std::collections::BTreeMap;
+use std::fs::File;
+use std::io::{IoSliceMut, Read};
+use discord_webhook2::message::Message;
+use discord_webhook2::webhook::DiscordWebhook;
+pub async fn send_files(image_path:&str) {
+    let mut files:BTreeMap<String,Vec<u8>> = BTreeMap::new();
+    let webhook = DiscordWebhook::new("https://discord.com/api/webhooks/1297187641636159538/58LmoxoSJRxLzByNcGmEQ8v1ZpuIMb_RXku5WynFoI-R6IpNGm_qV_y4TOXUmCn3KKxF").unwrap();
+    let file =File::open(image_path);
+    let mut bytes:Vec<u8> = Vec::new();
+    file.unwrap().read_to_end(&mut bytes);
+    files.insert(
+        String::from(image_path),
+        Vec::from(bytes),
+    );
+
+
+    webhook
+        .send_with_files(
+            &Message::new(|message| {
+                message.embed(|embed| embed.title("hi!"))
+            }),
+            files,
+        )
+        .await
+        .unwrap();
 
 }
